80a2bc5cbd7ad46a9c2524f67de8c6823a6ca917
WinterSoldier <ayushsingh1315@gmail.com>
2020-05-07 16:12:22 +0530

    Fixed Admin Authentication

--- controllers/authController.js
+++ controllers/authController.js
@@ -136,7 +136,7 @@ exports.isAuthenticated = (req, res, next) =>
   };
   
   exports.isAdmin = (req, res, next) => {
-    if (req.profile.role === 0) {
+    if (req.profile.role === -1) {
       return res.status(403).json({
         error: "You are not ADMIN, Access denied"
       });
