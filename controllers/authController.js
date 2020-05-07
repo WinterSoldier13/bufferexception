6c82fb144b123584ea8ff6db74645411371015cb
WinterSoldier <ayushsingh1315@gmail.com>
2020-05-07 16:08:04 +0530

    added Authentication

--- controllers/authController.js
+++ controllers/authController.js
@@ -119,3 +119,27 @@ exports.isSignedIn = expressJwt({
     secret: process.env.SECRET,
     userProperty: "auth"
   });
+
+
+//custom middlewares
+
+// Is Authenticated is used to see whether the user can make changes to his account only
+exports.isAuthenticated = (req, res, next) =>
+ {
+    let checker = req.profile && req.auth && req.profile._id == req.auth._id;
+    if (!checker) {
+      return res.status(403).json({
+        error: "ACCESS DENIED"
+      });
+    }
+    next();
+  };
+  
+  exports.isAdmin = (req, res, next) => {
+    if (req.profile.role === 0) {
+      return res.status(403).json({
+        error: "You are not ADMIN, Access denied"
+      });
+    }
+    next();
+  };
